96309985d472581c8db9df54ef595011c362889d
CptPackage <abdelrhmanabdalla15@gmail.com>
2020-02-19 19:09:58 +0100

    Created server-mode control

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -10,18 +10,33 @@ import logic.gui.MainGui;
 import torverbook.web.server.WebServer;
 
 public class Main {
-	
+
 	private static String driverClass = "com.mysql.jdbc.Driver";
-	
+	private static final String WEB_SERVER_MODE_FLAG = "--server-mode";
+
 	public static void main(String[] args) throws IOException, LifecycleException, URISyntaxException {
 		try {
-			Class.forName(driverClass);	
+			Class.forName(driverClass);
 		} catch (ClassNotFoundException e) {
 			System.exit(25);
 		}
-		
-		WebServer server = new WebServer(6090);
-		server.createServerEmbeddedWithXML();
-//		Application.launch(MainGui.class, args);
+
+		boolean webServerMode = serverMode(args);
+
+		if (webServerMode) {
+			WebServer server = new WebServer(5080);
+			server.createServerEmbeddedWithXML();
+		} else {
+			Application.launch(MainGui.class, args);
+		}
+	}
+
+	private static boolean serverMode(String[] args) {
+		for (String arg : args) {
+			if (arg.trim().contains(WEB_SERVER_MODE_FLAG)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
